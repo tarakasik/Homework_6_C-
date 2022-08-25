f3b35cb356205785f4af42ddb1527faeedd98cfd
Stanislav Volokhov <lamantina123@gmail.com>
2022-08-26 00:58:01 +0400

    add Task#41

---
+++ Task#41/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+Console.Clear();
+Console.Write("Input numbers separated by a space ' ': ");
+string[] userNumbers = Console.ReadLine()!.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+int positiveNumbers = 0;
+
+for (int i = 0; i < userNumbers.Length; i++)
+{
+    if (int.Parse(userNumbers[i]) > 0)
+    {
+        positiveNumbers++;
+    }
+}
+Console.WriteLine($"Numbers > 0 : {positiveNumbers}");
